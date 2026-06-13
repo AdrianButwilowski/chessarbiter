aaf07c9a47e969060817ba98946a0ca50b4464f0
Adrian Butwiłowski <adibu783@wp.pl>
2026-06-13 08:02:11 +0200

    inicjalizacja projektu Maven + główna klasa Spring Boot

---
+++ backend-java/src/main/java/pl/chessarbiter/ChessarbiterBackendApplication.java
@@ -0,0 +1,11 @@
+package pl.chessarbiter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessarbiterBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ChessarbiterBackendApplication.class, args);
+    }
+}
